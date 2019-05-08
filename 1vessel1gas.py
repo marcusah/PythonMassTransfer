6a2fcde26b75e2597bded36a2531a2d4e8c9e578
marcusah <makasuh@gmail.com>
2019-05-08 17:09:55 -0600

    Add files via upload

---
+++ 1vessel1gas.py
@@ -0,0 +1,257 @@
+# Calculating Mass Flow Rate 
+import math
+import csv
+
+#from decimal import *
+
+#clear all; close all;
+
+######################################Containment constants####################
+
+#Givens
+#Units are given in square brackets.
+R = 2076.9; # Helium gas constant [J/Kg/K]
+c_p = 5190.1; # Specific Heat capacity under constant P for He [J/Kg/K]
+c_v = 3122.0; # Specific Heat capacity under constant V for He[J/Kg/K]
+
+c_w = 500; # Specific Heat capacity for Vessel and internals [J/Kg/K]
+V_w = 0.018; # Volume of vessel structure & internals [m^3] (This should actually be 0.02 m^3)
+rho_w = 8000;# density of vessel structure and internals [kg/m^3]
+
+As= 1.413;# Total surface area [m^2]
+#V=265.9;
+V = 0.120;# Vessel free volume [m^3]
+
+Po = 2.5e8;# steady stay reactor power [W]
+
+gamma = 5.0/3; # ratio of specific heats for He [*unitless*]
+T_amb = 300.0;# Ambiant or confinment Temp [K]
+rho_amb = 0.13359;# ambiant He density[kg/m^3]
+mu_amb = 1.96e-5;# ambiant He viscosity [kg/m/s or Pa*s]
+a = math.sqrt(gamma*R*T_amb); #speed of sound at exit of pipe [m/s]
+p_amb = 101325.0; #ambiant pressure [Pa]
+
+mu_i = 3.84e-5;#initial He viscosity [Pa*s]
+m_i = 0.173;# intial He Mass in vessel [kg]
+rho_i = m_i/V;# intial helium density [kg/m^3]
+p_i = 1.724e6;#1 initial vessel pressure [Pa]
+
+Ti = 773.0; # initial helium temp [K]
+To = 273.0; # absolute temp [K]
+Twi = 773.0;# initial wall temp [K]
+
+eps = 4.5e-5;# pipe equivalant roughness for a new steel pipe [m]
+L = 0.0127; # length of pipe [m]
+d = 0.0127;# diameter of hot duct [m]
+# d2 = 1.5;
+# d3 = 2.29;
+Ac = math.pi/4*(d**2);# flow area of duct [m^2]
+Pw =math.pi*(d);# Wetted perimeter of duct [m]
+D = 4*Ac/Pw;#hydrolic diameter [m]
+K_c = 0.5;# Loss coefficent for sudden contraction [unitless]
+f_g = 0.002;#friction factor guess []
+alpha = 1;# kentic energy coefficents []
+
+tol=1e-6;#tolerance
+t_max = 10.0;# duration of blowdown transient [s]
+delta_t =0.001;#time step [s]
+n = int(math.floor(t_max/delta_t));# number of time steps
+err = 1;
+err1 = 1;
+
+
+
+h = 2;# heat transfer coefficent [W/m^2/K]
+#h = 10;
+#h=40;
+
+
+
+T = Ti;
+Ttemp = T
+Tw = Twi;
+Twtemp = Tw
+p = p_i;
+m = m_i;
+rho = rho_i;
+mu = mu_i;
+t=0 ;
+rho_avg= 0;
+mu_avg = 0;
+v_g = 0 ;
+Re_g = 0 ;
+f1 = 0 ;
+f = 0 ;
+v= 0;
+m_dot=0 ;
+Re=0 ;
+
+#User defined
+h = 2;
+
+err = 1;
+err1 = 1;
+
+mdot_array= [0];
+t_array = [0];
+T_array = [Ti];
+Tw_array = [Twi];
+p_array = [p_i];
+vg_array = [0];
+v_array = [0];
+m_array = [m_i];
+
+for i in range(1,n):
+
+    t = ((i-1)*delta_t);
+    rho_avg = (rho + rho_amb)/2;
+    mu_avg = (mu  + mu_amb)/2;
+  
+    while abs(err) >= tol:
+        v_g =  math.sqrt((p  - p_amb)/(rho_avg*(alpha / 2.0  + 0.5*(K_c + f_g*L/D))));
+        if v_g  >= a: 
+            Re_g = (rho_avg * a * D/mu_avg);
+        else :
+            Re_g = (rho_avg * v_g  * D/mu_avg);
+        #end
+        if Re_g  >= 4000:
+            f1 = ( 0.184*Re_g  **(-0.2));
+            while abs(err1) >=tol:
+                f = ( (1/(-2*math.log(eps/D)/3.7 + 2.51/(Re_g *math.sqrt(f1)))) **2);
+                err1 = f1  - f ;
+                f1  = f ;
+
+            #end
+        else:
+               f  = 64.0/Re_g ;
+        #end
+                
+        #print "t=  ", t 
+        v = (math.sqrt((p - p_amb)/(rho_avg *(alpha/2.0 + 0.5*(K_c + f *L/D)))));
+        err = abs(v_g  - v) ;
+        f_g = f ;
+
+        
+    #end
+    if v  >= a:
+        m_dot = ( rho_avg * Ac * a);
+        Re = ( rho_avg * a * D / mu_avg);
+        v = a;
+    else :
+        m_dot = ( rho_avg * Ac * v);
+        Re = ( rho_avg * v * D / mu_avg);
+    #end
+    m = ( m  - m_dot  * delta_t);
+    T = ( delta_t * h * As / (c_v * m )*Twtemp  + (1 - delta_t*(h * As + m_dot *R)/(c_v * m ))*Ttemp );
+    Tw = (  delta_t * 0.066 * Po *((i)*delta_t + (delta_t **2) **(-0.2))/(rho_w * V_w *c_w) + delta_t * h * As/(rho_w * V_w *c_w)*Ttemp  + ( 1 - delta_t * h * As / (rho_w * V_w *c_w))*Twtemp);
+    t = ( t  + delta_t);
+    Twtemp = Tw
+    Ttemp = T
+    mu = ( 1.865e-5 * (T/To) **(0.7));
+    rho = ( rho  - m_dot  * delta_t/V);
+    p = (rho  * R * T);
+    
+    #store wanted values
+    t_array.append(t);
+    mdot_array.append(m_dot);
+    m_array.append(m);
+    T_array.append(T);
+    Tw_array.append(Tw);
+    p_array.append(p);
+    vg_array.append(v_g);
+    v_array.append(v);
+    
+    if p  <= p_amb:
+        break
+    #end
+    err = err + 1;
+    err1 += 1;
+
+
+
+#end		
+			
+			
+
+#h2 = 10;
+#h3 = 40;
+
+#(t,T,Tw,m_dot1) = mass_transfer(h)
+#(T2,Tw2) = mass_transfer(h2)
+#(T3,Tw3) = mass_transfer(h3)
+
+# print "T =  ", T 
+   
+#print "Re_g =  ", Re_g 
+#print "t=  ", t
+# print "Tw =  ", Tw
+# for i in mdot_array:
+#     print "m_dot =  ", i
+# for i in t_array:
+#     print "t =  ", i
+# for i in T_array:
+#     print "T =  ", i
+# for i in Tw_array:
+#     print "tw =  ", i
+# print "Length of m_dot array = ", len(mdot_array)
+# print "Length of t array = ", len(t_array)
+# print "Length of T array = ", len(T_array)
+# print "Length of Tw array = ", len(Tw_array)
+# print "Length of p array = ", len(p_array)
+# print "Length of v_g array = ", len(vg_array)
+#print "t[333] = ", t_array[333]
+
+
+#write data to csv file
+mdotData = [t_array,mdot_array];
+vgData = [t_array,vg_array];
+vData = [t_array,v_array];
+pData = [t_array,p_array];
+mData = [t_array,m_array];
+TData = [t_array,T_array];
+
+with open('mdot.csv', 'w') as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerows(mdotData)
+
+csvFile.close()
+
+with open('vg.csv', 'w') as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerows(vgData)
+    
+csvFile.close()
+
+with open('p.csv', 'w') as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerows(pData)
+    
+csvFile.close()
+
+with open('v.csv', 'w') as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerows(vData)
+    
+csvFile.close()
+
+with open('m.csv', 'w') as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerows(mData)
+    
+csvFile.close()
+
+with open('T.csv', 'w') as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerows(TData)
+    
+csvFile.close()
+
+# print "rho_avg =  ", rho_avg
+
+print "max of vg_array =",max(vg_array)
+print "max of v_array =",max(v_array)
+# print "max of t_array =",max(t_array)
+print "t = ", t
+			
+			
+			
---
+++ 2gas2closed.py
@@ -0,0 +1,538 @@
+# Calculating Mass Flow Rate 
+import math
+import csv
+
+#from decimal import *
+
+#clear all; close all;
+
+#############################Containment constants#############################
+
+
+
+#Givens
+#Units are given in square brackets.
+
+
+
+############################# Needed Constants ###############################
+
+c_w = 500; # Specific Heat capacity for Vessel and internals [J/Kg/K]
+rho_w = 8000;# density of vessel structure and internals [kg/m^3]
+Po = 0.0 #2.5e8;# steady stay reactor power [W]
+tol=1e-6;#tolerance
+t_max = 10.0;# duration of blowdown transient [s]
+delta_t =0.001;#time step [s]
+n = int(math.floor(t_max/delta_t));# number of time steps
+alpha = 1;# kentic energy coefficents []
+K_c = 0.5;# Loss coefficent for sudden contraction [unitless]
+eps = 4.5e-5;# pipe equivalant roughness for a_v1 new steel pipe [m]
+To = 273.0; # absolute temp [K]
+p_amb = 101325.0; #ambiant pressure [Pa]
+T_amb = 300.0;# Ambiant or confinment Temp [K]
+h = 2;# heat transfer coefficent [W/m^2/K]
+t = 0;
+t_array = [0];
+#h = 10;
+#h=40;
+R_He = 2076.9; # Helium gas constant [J/Kg/K]
+R_Air = 287.05; # Air gas constant [J/Kg/K]
+c_pHe = 5190.1; # Specific Heat capacity under constant P for He [J/Kg/K]
+c_vHe = 3122.0; # Specific Heat capacity under constant V for He[J/Kg/K]
+c_pAir = 1.01e3; # Specific Heat capacity under constant P for Air [J/Kg/K]
+c_vAir = 718.0; # Specific Heat capacity under constant V for Air[J/Kg/K]
+
+
+mu_ambHe = 1.96e-5;# ambiant He viscosity [kg/m/s or Pa*s]
+mu_ambAir = 1.82e-5;# ambiant Air viscosity [kg/m/s or Pa*s]
+
+
+########################## For Vessel 1: The PV ########################################
+
+# Gas properties
+R_v1 = R_He # 2076.9; # Helium gas constant [J/Kg/K]
+c_p_v1 = c_pHe #5190.1; # Specific Heat capacity under constant P for He [J/Kg/K]
+c_v_v1 = c_vHe #3122.0; # Specific Heat capacity under constant V_v1 for He[J/Kg/K]
+
+gamma_v1 = c_p_v1/c_v_v1; # ratio of specific heats for He [*unitless*]
+rho_amb_v1 = 0.13359;# ambiant He density[kg/m_v1^3]
+mu_amb_v1 = 1.96e-5;# ambiant He viscosity [kg/m_v1/s or Pa*s]
+
+
+# physical properties of vessel
+V_w_v1 = 0.02; # Volume of vessel structure & internals [m^3] 
+As_v1= 1.413;# Total surface area [m^2]
+V_v1 = 0.120;# Vessel free volume [m^3]
+
+
+
+# Mass, Mu, Pressure, and Rho
+mu_i_v1 = 3.84e-5;#initial He viscosity [Pa*s]
+m_i_v1 = 0.173;# intial He Mass in vessel [kg]
+rho_i_v1 = m_i_v1/V_v1;# intial helium density [kg/m_v1^3]
+p_i_v1 = 1.724e6;# initial vessel pressure [Pa]
+
+
+
+# Tempurature
+Ti_v1 = 773.15; # initial helium temp [K]
+Twi_v1 = 773.15;# initial wall temp [K]
+p_i_v1 = rho_i_v1*R_v1*Ti_v1
+
+
+# Duct properties
+L_v1 = 0.0127; # length of pipe [m]
+d_v1 = 0.0127;# diameter of hot duct [m]
+Ac_v1 = math.pi/4*(d_v1**2);# flow area of duct [m^2]
+Pw_v1 =math.pi*(d_v1);# Wetted perimeter of duct [m]
+D_v1 = 4*Ac_v1/Pw_v1;#hydrolic diameter [m]
+f_g_v1 = 0.002;#friction factor guess []
+a_v1 = math.sqrt(gamma_v1*R_v1*T_amb); #speed of sound at exit of pipe [m/s]
+
+
+
+# Error 
+err_v1 = 1;
+err1_v1 = 1;
+
+
+# Initial Conditions
+T_v1 = Ti_v1;
+T_v1temp = Ti_v1;
+Tw_v1 = Twi_v1;
+Tw_v1temp = Tw_v1
+p_v1 = p_i_v1;
+m_v1 = m_i_v1;
+m_v1temp = m_v1
+rho_v1 = rho_i_v1;
+mu_v1 = mu_i_v1;
+rho_avg_v1= 0;
+mu_avg_v1 = 0;
+v_g_v1 = 0 ;
+Re_g_v1 = 0 ;
+f1_v1 = 0 ;
+f_v1 = 0 ;
+v_v1= 0;
+m_dot_v1=0 ;
+Re_v1=0 ;
+delta_u_v1 =0;
+
+
+# Initialize arrays for storage
+mdot_array_v1= [0];
+T_array_v1 = [Ti_v1];
+Tw_array_v1 = [Twi_v1];
+p_array_v1 = [p_i_v1];
+vg_array_v1 = [0];
+v_array_v1 = [0];
+du_array_v1 = [delta_u_v1];
+m_array_v1 = [m_i_v1]
+
+
+
+
+########################## Vessel 2: The PVC ########################################
+
+
+# Gas properties
+R_v2 = 287.05; # V2 gas constant [J/Kg/K]
+c_p_v2 =  1.01e3; # Specific Heat capacity under constant P for V2 [J/Kg/K]
+c_v_v2 = 718.0; # Specific Heat capacity under constant V_v1 for V2[J/Kg/K]
+c_p_v2temp = c_pAir;
+c_v_v2temp = c_vAir;
+
+gamma_v2 = c_p_v2/c_v_v2; # ratio of specific heats for He [*unitless*]
+rho_amb_v2 = 1.225;# ambiant He density[kg/m_v1^3]
+mu_amb_v2 = 1.813e-5;# ambiant He viscosity [kg/m_v1/s or Pa*s]
+a_v2 = math.sqrt(gamma_v2*R_v2*T_amb); #speed of sound at exit of pipe [m/s]
+
+
+
+# Physical properties of vessel
+V_w_v2 = 0.02; # Volume of vessel structure & internals [m^3] 
+As_v2= 24971.0/1550.02;# Total surface area [m^2]
+V_v2 = 0.232;# Vessel free volume [m^3]
+
+
+
+# Mass, Mu, Pressure, and Rho
+mu_i_v2 = 3.64e-5;#initial He viscosity [Pa*s]
+rho_i_v2 = 0.4567;# intial helium density [kg/m_v1^3]
+m_i_v2 = rho_i_v2*V_v2;# intial He Mass in vessel [kg]
+p_i_v2 = 101325;# initial vessel pressure [Pa]
+
+
+
+# Tempurature
+Ti_v2 = 773.15; # initial helium temp [K]
+Twi_v2 = 773.15;# initial wall temp [K]
+
+
+# Duct properties
+L_v2 = 0.0; # length of pipe [m]
+d_v2 = 0.0929;# diameter of hot duct [m]
+Ac_v2 = math.pi/4*(d_v2**2);# flow area of duct [m^2]
+Pw_v2 =math.pi*(d_v2);# Wetted perimeter of duct [m]
+D_v2 = (24.0/13.0)/39.37;#hydrolic diameter [m]
+f_g_v2 = 0.002;#friction factor guess []
+
+
+# Initial Conditions
+T_v2 = Ti_v2;
+T_v2temp = Ti_v2;
+Tw_v2 = Twi_v2;
+Tw_v2temp = Tw_v2;
+p_v2 = p_i_v2;
+m_v2 = m_i_v2;
+m_v2temp = m_v2;
+rho_v2 = rho_i_v2;
+mu_v2 = mu_i_v2;
+rho_avg_v2= 0;
+mu_avg_v2 = 0;
+v_g_v2 = 0 ;
+Re_g_v2 = 0 ;
+f1_v2 = 0 ;
+f_v2 = 0 ;
+v_v2= 0;
+m_dot_v2=0 ;
+Re_v2=0 ;
+delta_u_v2 = 0; 
+
+rho_amb_v1 = rho_i_v2;# ambiant He density[kg/m_v1^3]
+mu_amb_v1 = mu_i_v2;# ambiant He viscosity [kg/m_v1/s or Pa*s]
+
+
+#Helium amount in vessel 2
+He_v2 = 0
+
+# Error 
+err_v2 = 1;
+err1_v2 = 1;
+
+
+# Initialize arrays for storage
+mdot_array_v2= [0];
+T_array_v2 = [Ti_v2];
+Tw_array_v2 = [Twi_v2];
+p_array_v2 = [p_i_v2];
+vg_array_v2 = [0];
+v_array_v2 = [0];
+du_array_v2 = [delta_u_v2];
+m_array_v2 = [m_i_v2]
+cp_array_v2 = [c_p_v2]
+
+
+################################## Run Blowdown  ############################
+
+
+for i in range(1,n+1):
+
+    t += (delta_t);# update total time
+    
+####################### Vessel 1 calculations#########################
+    
+    a_v1 = math.sqrt(gamma_v1*R_v1*T_v2); # Recalulate new sound speed.
+    rho_avg_v1 = (rho_v1 + rho_amb_v1)/2; # Recalculate new rho avg
+    mu_avg_v1 = (mu_v1  + mu_amb_v1)/2;
+  
+    while abs(err_v1) >= tol:
+        v_g_v1 =  math.sqrt((p_v1  - p_amb)/(rho_avg_v1*(alpha / 2.0  + 0.5*(K_c + f_g_v1*L_v1/D_v1))));
+        if v_g_v1  >= a_v1: 
+            Re_g_v1 = (rho_avg_v1 * a_v1 * D_v1/mu_avg_v1);
+        else :
+            Re_g_v1 = (rho_avg_v1 * v_g_v1  * D_v1/mu_avg_v1);
+        #end
+        if Re_g_v1  >= 4000:
+            f1_v1 = ( 0.184*Re_g_v1  **(-0.2));
+            while abs(err1_v1) >=tol:
+                f_v1 = ( (1/(-2*math.log(eps/D_v1)/3.7 + 2.51/(Re_g_v1 *math.sqrt(f1_v1)))) **2);
+                err1_v1 = f1_v1  - f_v1 ;
+                f1_v1  = f_v1 ;
+
+            #end
+        else:
+               f_v1  = 64.0/Re_g_v1 ;
+        #end
+                
+        #print "t=  ", t 
+        v_v1 = (math.sqrt((p_v1 - p_amb)/(rho_avg_v1 *(alpha/2.0 + 0.5*(K_c + f_v1 *L_v1/D_v1)))));
+        err_v1 = abs(v_g_v1  - v_v1) ;
+        f_g_v1 = f_v1 ;
+
+        
+    #end
+    if v_v1  >= a_v1:
+        m_dot_v1 = ( rho_avg_v1 * Ac_v1 * a_v1);
+        Re_v1 = ( rho_avg_v1 * a_v1 * D_v1 / mu_avg_v1);
+        v_v1 = a_v1;
+    else :
+        m_dot_v1 = ( rho_avg_v1 * Ac_v1 * v_v1);
+        Re_v1 = ( rho_avg_v1 * v_v1 * D_v1 / mu_avg_v1);
+    #end
+    
+    m_v1 -=  m_dot_v1  * delta_t;
+    
+    T_v1 =  delta_t * h * As_v1 / (c_v_v1 * m_v1temp )*Tw_v1temp  + (1 - delta_t*(h * As_v1 + m_dot_v1 *h)/(c_v_v1 * m_v1temp ))*T_v1temp ;
+    delta_u_v1 +=  m_v1  * c_p_v1 * (T_v1 - T_v1temp)
+    Tw_v1 =   (delta_t * 0.066 * Po *( t - delta_t + delta_t ** 2) ** (-0.2))/(rho_w * V_w_v1 *c_w) - delta_t * h * As_v1/(rho_w * V_w_v1 *c_w)*T_v1temp  + ( 1 + delta_t * h * As_v1 / (rho_w * V_w_v1 *c_w))*Tw_v1temp;
+
+    mu_v1 = ( mu_amb_v1 * (T_v1/To) **(0.7));
+    rho_v1 = m_v1/V_v1;
+    p_v1 = (rho_v1  * R_v1 * T_v1);
+    
+    T_v1temp = T_v1;
+    Tw_v1temp = Tw_v1;
+    m_v1temp = m_v1;
+    
+    ####################### Vessel 2 calculations#########################
+    
+    
+    # rho_avg_v2 = (rho_v2 + rho_amb_v2)/2;
+    # mu_avg_v2 = (mu_v2  + mu_amb_v2)/2;
+  
+    # # if p_v2 < p_amb:
+    # #     p_v2 = p_amb
+        
+    # v_g_v2 =  math.sqrt((p_v2  - p_amb)/(rho_avg_v2*(alpha + K_c)/2.0 ));
+
+    
+    # if v_g_v2  > a_v2: 
+    #     v_g_v2 = a_v2;
+        
+    # else :
+    #     pass
+    #end
+    # v_v2 = v_g_v2;
+    #end
+    v_v2 = 0
+    m_dot_v2 = 0 #( rho_avg_v2 * Ac_v2 * v_v2);
+    #end
+    
+    
+    m_v2 +=  m_dot_v1 * delta_t ; 
+    He_v2= (He_v2 * m_v2temp +  m_dot_v1 * delta_t)/m_v2
+    c_p_v2 = c_pAir*(1 - He_v2) + c_pHe*He_v2
+    c_v_v2 = c_vAir*(1-He_v2) + c_vHe * He_v2
+    R_v2 = R_Air*(1-He_v2) + R_He * He_v2
+    # mu_amb_v1 = mu_ambAir*(1-He_v2) + mu_ambHe * He_v2
+    
+    T_v2 =  delta_t * h * As_v2 / (c_v_v2temp * m_v2temp )*Tw_v2temp  + T_v2temp #*(1 - delta_t*(h * As_v2 )/(c_v_v2 * m_v2temp ))*T_v2temp ;
+    delta_u_v2 = delta_u_v2 +  m_v2  * c_p_v2 * (T_v2-T_v2temp);
+    Tw_v2 = delta_t * h * As_v2/(rho_w * V_w_v2 *c_w)*T_v2temp  + ( 1 - delta_t * h * As_v2 / (rho_w * V_w_v2 *c_w))*Tw_v2temp;
+
+    mu_v2 =  (mu_ambAir*(1-He_v2) + mu_ambHe * He_v2) * (T_v2/To) ** (0.7);
+    rho_v2 = m_v2/V_v2;
+    p_v2 = rho_v2  * R_v2 * T_v2;
+    
+    T_v2temp = T_v2;
+    Tw_v2temp = Tw_v2;
+    m_v2temp = m_v2;
+    c_v_v2temp = c_v_v2
+    c_p_v2temp = c_p_v2
+    
+    rho_amb_v1 = rho_v2
+    mu_amb_v1 = mu_v2
+    
+    
+    
+    
+    #store wanted values
+    t_array.append(t);
+    
+    
+    mdot_array_v1.append(m_dot_v1);
+    m_array_v1.append(m_v1);
+    T_array_v1.append(T_v1);
+    Tw_array_v1.append(Tw_v1);
+    p_array_v1.append(p_v1);
+    vg_array_v1.append(v_g_v1);
+    v_array_v1.append(v_v1);
+    du_array_v1.append(delta_u_v1);
+    
+    
+    mdot_array_v2.append(m_dot_v2);
+    m_array_v2.append(m_v2);
+    T_array_v2.append(T_v2);
+    Tw_array_v2.append(Tw_v2);
+    p_array_v2.append(p_v2);
+    vg_array_v2.append(v_g_v2);
+    v_array_v2.append(v_v2);
+    du_array_v2.append(delta_u_v2);
+    cp_array_v2.append(c_p_v2);
+    
+    if p_v1  <= p_amb:
+        print "p_v1 <= p_amb"
+        print "t = ",t
+        break
+    #end
+    if p_v1  <= p_v2:
+        print "p_v1 <= p_v2"
+        print "t = ",t
+        break
+    #end
+    
+    # if p_v2  <= p_amb:
+    #     print "p_v2 <= p_amb"
+    #     print "t = ",t
+    #     break
+    #end
+    
+    if T_v1 <0 or T_v2<0:
+        print "error. Temp below 0 K"
+        break
+    
+    if t> t_max:
+        print "t > tmax"
+        break
+    
+    # if p_v2  <= p_amb:
+    #     print "p_v2 <= p_amb"
+    #     print "t = ",t
+    #     break
+    # # end
+    
+    # if p_v1  <= p_v2:
+    #     print "p_v1 <= p_v2"
+    #     print "t = ",t
+    #     break
+    
+    err_v1 = err_v1 + 1;
+    err1_v1 += 1;
+
+
+
+#end		
+
+# print "T_v1 =  ", T_v1 
+   
+#print "Re_g_v1 =  ", Re_g_v1 
+#print "t=  ", t
+# print "Tw_v1 =  ", Tw_v1
+# for i in mdot_array_v1:
+#     print "m_dot_v1 =  ", i
+# for i in t_array_v1:
+#     print "t =  ", i
+# for i in T_array_v1:
+#     print "T_v1 =  ", i
+# for i in Tw_array_v1:
+#     print "tw =  ", i
+# print "Length of m_dot_v1 array = ", len(mdot_array_v1)
+# print "Length of t array = ", len(t_array_v1)
+# print "Length of T_v1 array = ", len(T_array_v1)
+# print "Length of Tw_v1 array = ", len(Tw_array_v1)
+# print "Length of p_v1 array = ", len(p_array_v1)
+# print "Length of v_g_v1 array = ", len(vg_array_v1)
+#print "t[333] = ", t_array_v1[333]
+
+
+###################### write data to csv file ########################
+
+#                 Vessel 1
+
+mdot_Data_v1 = [t_array,mdot_array_v1];
+m_Data_v1 = [t_array,m_array_v1];
+du_Data_v1 = [t_array,du_array_v1];
+vData_v1 = [t_array,v_array_v1];
+pData_v1 = [t_array,p_array_v1];
+T_Data_v1 = [t_array,T_array_v1];
+
+
+with open('mdotV1.csv', 'w') as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerows(mdot_Data_v1)
+
+csvFile.close()
+
+with open('duV1.csv', 'w') as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerows(du_Data_v1)
+    
+csvFile.close()
+
+with open('pV1.csv', 'w') as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerows(pData_v1)
+    
+csvFile.close()
+
+with open('vV1.csv', 'w') as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerows(vData_v1)
+    
+csvFile.close()
+
+
+with open('TV1.csv', 'w') as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerows(T_Data_v1)
+    
+csvFile.close()
+
+with open('mV1.csv', 'w') as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerows(m_Data_v1)
+    
+csvFile.close()
+
+
+#             Vessel 2
+
+mdot_Data_v2 = [t_array,mdot_array_v2];
+du_Data_v2 = [t_array,du_array_v2];
+vData_v2 = [t_array,v_array_v2];
+pData_v2 = [t_array,p_array_v2];
+m_Data_v2 = [t_array,m_array_v2];
+T_Data_v2 = [t_array,T_array_v2];
+cp_Data_v2 = [t_array, cp_array_v2]
+
+with open('mdotV2.csv', 'w') as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerows(mdot_Data_v2)
+
+csvFile.close()
+
+with open('duV2.csv', 'w') as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerows(du_Data_v2)
+    
+csvFile.close()
+
+with open('pV2.csv', 'w') as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerows(pData_v2)
+    
+csvFile.close()
+
+with open('vV2.csv', 'w') as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerows(vData_v2)
+    
+csvFile.close()
+
+with open('TV2.csv', 'w') as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerows(T_Data_v2)
+    
+csvFile.close()
+
+with open('mV2.csv', 'w') as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerows(m_Data_v2)
+    
+csvFile.close()
+
+with open('cpV2.csv', 'w') as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerows(cp_Data_v2)
+    
+csvFile.close()
+
+# print "rho_avg_v1 =  ", rho_avg_v1
+
+#print "max of vg_array_v1 =",max(vg_array_v1)
+#print "max of v_array_v1 =",max(v_array_v1)
+# print "max of t_array_v1 =",max(t_array_v1)
+#print "a_v1 = ", a_v1
+			
+			
+			
